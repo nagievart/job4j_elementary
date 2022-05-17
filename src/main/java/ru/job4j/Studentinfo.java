1310a2286e2e0d806749475a9e8a7402e5a3e305
Артур Нагиев <nagiev-art@mail.ru>
2022-05-17 19:42:00 +0300

    Загрузка файлов на github [#243562 #301698]

--- src/main/java/ru/job4j/Studentinfo.java
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class Studentinfo {
-        public static void main(String[] args){
+        public static void main(String[] args) {
             System.out.println("Artur Nagiev");
             System.out.println("25.08.1995");
     }
