402403982e5c00760dfddc5154f967e3427ba028
Артур Нагиев <nagiev-art@mail.ru>
2022-05-19 20:54:26 +0300

    Загрузка файлов на github [#243562 #301698]

--- src/main/java/calculator/MathFunc.java
+++ src/main/java/calculator/MathFunc.java
@@ -6,14 +6,8 @@ public class MathFunc {
         return y;
     }
 
-    public static int func2(int x) {
-        int y = 1 / x;
-        return y;
-    }
-
     public static void main(String[] args) {
         int result1 = MathFunc.func1(100);
-        int result2 = MathFunc.func1(5);
         int result3 = result1;
     }
 }
