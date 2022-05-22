725f5e7ec7843fa5476e041387b8d2d564c00abd
Артур Нагиев <nagiev-art@mail.ru>
2022-05-22 09:05:50 +0300

    Загрузка файлов на github [#243562 #301698]

---
+++ src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class Point {
+    public static double distance(int x1, int y1, int x2, int y2) {
+        int differenceX = x1 - x2;
+        int differenceY = y1 - y2;
+        double degreeX = Math.pow(differenceX, 2);
+        double degreeY = Math.pow(differenceY, 2);
+        double sum = degreeX + degreeY;
+        double rsl = Math.sqrt(sum);
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        double result = Point.distance(0, 0, 2, 0);
+        System.out.println("result (0, 0) to (2, 0) " + result);
+    }
+}
