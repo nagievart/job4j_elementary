1f21535414cd343e98ae642df68d60cc8374933d
Артур Нагиев <nagiev-art@mail.ru>
2022-05-23 21:18:29 +0300

    Загрузка файлов на github [#243562 #301698]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -16,5 +16,15 @@ public class Converter {
         System.out.println("1500 rubles are " + euro + " euro.");
         float dollar = Converter.rubleToDollar(1500);
         System.out.println("1500 rubles are " + dollar + " dollars.");
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        float input = 120;
+        float expectation = 2;
+        float output = Converter.rubleToDollar(input);
+        boolean pass = expectation == output;
+        System.out.println("120 rubles are 2. Test result : " + pass);
     }
 }
