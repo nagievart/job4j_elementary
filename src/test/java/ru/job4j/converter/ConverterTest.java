d6a93dfc34c48dddaf6bbcc601dac328e0f89453
Артур Нагиев <nagiev-art@mail.ru>
2022-05-23 21:49:53 +0300

    Загрузка файлов на github [#243562 #301698]

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.converter;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert210RblThen3Euro() {
+        float in = 210;
+        float expected = 3;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert240RblThen4Dlr() {
+        float in = 240;
+        float expected = 4;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+}
