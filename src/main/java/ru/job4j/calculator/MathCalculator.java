a7710bf133b44fb8dfd64f08bd3c1f546db6c3d3
Артур Нагиев <nagiev-art@mail.ru>
2022-05-24 20:38:39 +0300

    Загрузка файлов на github [#243562 #301698]

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,22 @@
+package ru.job4j.calculator;
+
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+    }
+
+    public static double sumOfDivAndDiff(double first, double second) {
+        return diff(first, second) + div(first, second);
+    }
+
+    public static double sumOfAll(double first, double second) {
+        return sum(first, second) + multiply(first, second) + div(first, second) + diff(first, second);
+    }
+}
