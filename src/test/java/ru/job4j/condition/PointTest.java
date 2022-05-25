caf1fa7dd7c2a76b7146f86e678172f688024c3d
Артур Нагиев <nagiev-art@mail.ru>
2022-05-25 20:41:46 +0300

    Загрузка файлов на github [#243562 #301698]

---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PointTest {
+
+    @Test
+    public void when00to20then2() {
+        double expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when11to11then1dot41() {
+        double expected = 1.41;
+        int x1 = 1;
+        int y1 = 1;
+        int x2 = 2;
+        int y2 = 2;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when22to22then2dot83() {
+        double expected = 2.83;
+        int x1 = 1;
+        int y1 = 1;
+        int x2 = 3;
+        int y2 = 3;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when11to88then9dot9() {
+        double expected = 9.9;
+        int x1 = 1;
+        int y1 = 1;
+        int x2 = 8;
+        int y2 = 8;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertEquals(expected, out, 0.01);
+    }
+}
