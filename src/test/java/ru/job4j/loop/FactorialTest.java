f9067109f93da42b89a45ebf1ca306d1bf750048
Артур Нагиев <nagiev-art@mail.ru>
2022-06-05 21:13:25 +0300

    Загрузка файлов на github [#243562 #301698]

---
+++ src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FactorialTest {
+
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundredTwenty() {
+        int in = 5;
+        int expected = 120;
+        int out = Factorial.calc(in);
+        assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenCalculateFactorialForZeroThenOne() {
+        int in = 0;
+        int expected = 1;
+        int out = Factorial.calc(in);
+        assertEquals(expected, out);
+    }
+}
