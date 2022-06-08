adc2677ef0f4dbb76f54bdaea4f6ab2481026dcf
Артур Нагиев <nagiev-art@mail.ru>
2022-06-08 19:23:31 +0300

    Загрузка файлов на github [#243562 #301698]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,9 +2,9 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
+        boolean prime = number > 1;
         for (int i = 2; i <= number - 1; i++) {
-            if (number % 2 == 0 || number % (number - 1) == 0) {
+            if (number % i == 0) {
                 prime = false;
                 break;
             }
