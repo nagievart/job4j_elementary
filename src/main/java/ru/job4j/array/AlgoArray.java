e10edb23983c498d2e68c50bab3e23846f0611b6
Артур Нагиев <nagiev-art@mail.ru>
2022-06-14 19:40:46 +0300

    Загрузка файлов на github [#243562 #301698]

---
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+public class AlgoArray {
+    public static void main(String[] args) {
+        int[] array = new int[] {5, 3, 2, 1, 4};
+        int temp = array[0];
+        array[0] = array[3];
+        array[3] = temp;
+        for (int index = 0; index < array.length - 1; index++) {
+            if (array[index] > array[index + 1]) {
+                temp = array[index];
+                array[index] = array[index + 1];
+                array[index + 1] = temp;
+            }
+        }
+        for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
+        }
+    }
+}
