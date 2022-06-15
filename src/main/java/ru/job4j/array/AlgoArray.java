50d217a7f314f7a65c462bb9f1ffb93c4d73ce03
Артур Нагиев <nagiev-art@mail.ru>
2022-06-15 18:42:43 +0300

    Загрузка файлов на github [#243562 #301698]

--- src/main/java/ru/job4j/array/AlgoArray.java
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -6,13 +6,12 @@ public class AlgoArray {
         int temp = array[0];
         array[0] = array[3];
         array[3] = temp;
-        for (int index = 0; index < array.length - 1; index++) {
-            if (array[index] > array[index + 1]) {
-                temp = array[index];
-                array[index] = array[index + 1];
-                array[index + 1] = temp;
-            }
-        }
+        temp = array[1];
+        array[1] = array[2];
+        array[2] = temp;
+        temp = array[3];
+        array[3] = array[4];
+        array[4] = temp;
         for (int index = 0; index < array.length; index++) {
             System.out.println(array[index]);
         }
