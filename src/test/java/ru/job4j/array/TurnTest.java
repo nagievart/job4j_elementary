7772fe48b95ebf760b0c70239cd59c13e5ab84a2
Артур Нагиев <nagiev-art@mail.ru>
2022-06-19 17:10:57 +0300

    Загрузка файлов на github [#243562 #301698]

---
+++ src/test/java/ru/job4j/array/TurnTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TurnTest {
+
+    @Test
+    public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
+        int[] input = new int[] {4, 1, 6, 2};
+        int[] result = Turn.back(input);
+        int[] expected = new int[] {2, 6, 1, 4};
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenTurnArrayWithNotEvenAmountOfElementsThenTurnedArray() {
+        int[] input = new int[] {4, 1, 6, 2, 5};
+        int[] result = Turn.back(input);
+        int[] expected = new int[] {5, 2, 6, 1, 4};
+        assertArrayEquals(expected, result);
+    }
+}
