d4c4aa9de313f194b389d22a07ed29db45a24f93
Артур Нагиев <nagiev-art@mail.ru>
2025-01-01 20:33:23 +0300

    3.3. Фиксация изменений на github [#243564 #301719]

--- src/main/java/ru/job4j/Mynewclass.java
+++ src/main/java/ru/job4j/Mynewclass.java
@@ -3,5 +3,15 @@ package ru.job4j;
 public class Mynewclass {
     public static void main(String[] args) {
         System.out.println("Хочу проверить, как новый класс загрузится в Git.");
+        byte age = 29;
+        char sex = 'm';
+        float length = 1.7F;
+        int bricks = 100500;
+        double backtery = 2E+9;
+        double distance = 52E+9;
+        boolean jobPermission = true;
+        int size = 500000000;
+        System.out.println(age);
+        System.out.println(sex);
     }
 }
