23189cde7acdea33c6a685b59869013111d7af53
Артур Нагиев <nagiev-art@mail.ru>
2025-01-03 19:43:20 +0400

    3.3. Фиксация изменений на github [#243564 #301719]

--- src/main/java/ru/job4j/condition/RectangleArea.java
+++ src/main/java/ru/job4j/condition/RectangleArea.java
@@ -11,6 +11,6 @@ public class RectangleArea {
 
     public static void main(String[] args) {
         double result = RectangleArea.square(6, 2);
-        System.out.println(" p = 6, k = 2, s = 1, real = " + result);
+        System.out.println(" p = 6, k = 2, s = 2, real = " + result);
     }
 }
