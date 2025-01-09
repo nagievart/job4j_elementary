cd8ec8585707e0fd415f39fd7735815b8cf408be
Артур Нагиев <nagiev-art@mail.ru>
2025-01-09 20:02:38 +0300

    3.3. Фиксация изменений на github [#243564 #301719]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -4,7 +4,19 @@ public class ArrayDefinition {
 
     public static void main(String[] args) {
         short[] ages = new short[10];
+        System.out.println(ages.length);
         String[] surnames = new String[100500];
+        System.out.println(surnames.length);
         float[] pieces = new float[50];
+        System.out.println(pieces.length);
+        String[] names = new String[4];
+        names[0] = "Artur Nagiev";
+        System.out.println(names[0]);
+        names[1] = "Mariia Nagieva";
+        System.out.println(names[1]);
+        names[2] = "Ivan Ivanovich";
+        System.out.println(names[2]);
+        names[3] = "Boris Borisovich";
+        System.out.println(names[3]);
     }
 }
