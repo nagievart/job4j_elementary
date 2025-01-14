247dd402657d6077a5e2cd672da56eac62b57deb
Артур Нагиев <nagiev-art@mail.ru>
2025-01-14 19:39:09 +0300

    3.3. Фиксация изменений на github [#243564 #301719]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -5,7 +5,7 @@ public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
         for (int cell = 0; cell < board.length; cell++) {
-            if (board[row][cell] == ' ') {
+            if (board[row][cell] != 'X') {
                 result = false;
                 break;
             }
@@ -16,7 +16,7 @@ public class MatrixCheck {
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
         for (int row = 0; row < board.length; row++) {
-            if (board[row][column] == ' ') {
+            if (board[row][column] != 'X') {
                 result = false;
                 break;
             }
