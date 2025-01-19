b9b8aa92d0230c8f291b6b93f9ec66f81df4e4f9
Артур Нагиев <nagiev-art@mail.ru>
2025-01-19 09:52:19 +0300

    3.3. Фиксация изменений на github [#243564 #301719]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -8,14 +8,14 @@ public class Machine {
         int[] coins = {10, 5, 2, 1};
         int[] result = new int[100];
         int size = 0;
-        int changeAmount = money - price;
         for (int i = 0; i < coins.length; i++) {
-            while (changeAmount >= coins[i]) {
-                result[i] = coins[i];
-                changeAmount -= coins[i];
+            while (money - price >= coins[i]) {
+                result[size] = coins[i];
+                money -= coins[i];
                 size++;
             }
         }
         return Arrays.copyOf(result, size);
+
     }
 }
