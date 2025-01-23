4d49605f73e702892674d50e30c6cc9a6d591204
Артур Нагиев <nagiev-art@mail.ru>
2025-01-23 21:06:25 +0300

    3.3. Фиксация изменений на github [#243564 #301719]

---
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+public class SimpleStringEncoder {
+
+    public static String encode(String input) {
+        String result = "";
+        char symbol = input.charAt(0);
+        int counter = 1;
+        for (int i = 1; i < input.length(); i++) {
+            if (symbol == input.charAt(i)) {
+                counter++;
+            } else {
+                if (counter > 1) {
+                    result = result + symbol + counter;
+                } else {
+                    result = result + symbol;
+                }
+                symbol = input.charAt(i);
+                counter = 1;
+            }
+        }
+            if (counter > 1) {
+                result = result + symbol + counter;
+            } else {
+                result = result + symbol;
+        }
+        return result;
+    }
+}
