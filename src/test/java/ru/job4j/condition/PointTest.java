302a39920a7ec759c087c5d9d21f3d0c0dea1915
Артур Нагиев <nagiev-art@mail.ru>
2025-02-09 18:01:02 +0300

    3.3. Фиксация изменений на github [#243564 #301719]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -31,4 +31,29 @@ class PointTest {
         double expected = 5.099;
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
+
+    @Test
+    void distance3dWhen111to51015Then17Dot11() {
+        Point a = new Point(1, 1, 1);
+        Point b = new Point(5, 10, 15);
+        double output = a.distance3d(b);
+        double expected = 17.11;
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    void distance3dWhen51015to017Then13Dot03() {
+        Point a = new Point(5, 10, 15);
+        Point b = new Point(0, 1, 7);
+        double output = a.distance3d(b);
+        double expected = 13.03;
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    void distance3dWhen000to000Then0() {
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(0, 0, 0);
+        double output = a.distance3d(b);
+        double expected = 0;
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
 }
